9ddc511ff54f107bcdc2e77ccb427267b86eea19
Christopher Wallis <christopher.j.wallis@gmail.com>
2017-12-07 00:12:14 -0500

    adding versioning script

--- scripts/version.js
+++ scripts/version.js
@@ -5,13 +5,19 @@ const path = require('path')
 const ENCODING = 'utf-8'
 
 const readJSON = fileName => JSON.parse(fs.readFileSync(fileName), ENCODING)
-const version = readJSON(path.resolve(__dirname, './package.json')).version
+const version = readJSON(path.resolve(__dirname, '../package.json')).version
 
 // prettier-ignore
-fs.readdirSync('packages')
+fs.readdirSync(path.resolve(__dirname, '../packages'))
     .forEach(pkg => {
+        const dir = path.resolve(__dirname, '../packages', pkg)
+        if (!fs.lstatSync(dir).isDirectory()) {
+            // ignore files
+            return
+        }
+
         // read package json
-        const jsonFile = path.resolve(__dirname, './packages', pkg, 'package.json')
+        const jsonFile = path.resolve(dir, 'package.json')
         const json = readJSON(jsonFile)
 
         // update version
@@ -21,7 +27,5 @@ fs.readdirSync('packages')
         fs.writeFileSync(jsonFile, JSON.stringify(json, null, '  ') + '\n', ENCODING)
         console.log('\u2713 updated ' + pkg)
     })
-
-fs.writeFileSync(versionFile, versionFileContents, ENCODING)
 
 console.log('updated all versions')
